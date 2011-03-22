6f2115a18153c9065225d8a895edb3490c9a853d
Sylvester Keil <sylvester@keil.or.at>
2011-03-22 12:45:44 +0100

    Made iso8601 parsing more explicit in Duration.

--- lib/edtf/duration.rb
+++ lib/edtf/duration.rb
@@ -3,15 +3,22 @@ class EDTF
   class Duration < Struct.new(:year, :month, :week, :day, :hour, :minute, :second)
     include Comparable
 
+    ISO8601 = /^P(?:(\d+)Y)?(?:(\d+)M)?(?:(\d+)W)?(?:(\d+)D)?(?:T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?)?$/
+    
+    def self.iso8601(string); new(string); end
+    
     def initialize(*arguments)
-      arguments = iso8601(arguments[0]) if arguments.length == 1 && arguments[0].start_with?('P')
+      arguments = parse(arguments[0]) if arguments.length == 1 && arguments[0].to_s.start_with?('P')
       super
     end
 
-    def iso8601(duration)
-      duration.match(/^P(?:(\d+)Y)?(?:(\d+)M)?(?:(\d+)W)?(?:(\d+)D)?(?:T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?)?$/).captures.map(&:to_i)
+    def iso8601(argument = nil)
+      return to_s if argument.nil?
+      
+      members.zip(parse(argument)).each { |m,v| send("#{m}=", v.to_i) }
+      self
     end
-    
+        
     def <=>(other)
       values.map(&:to_i) <=> other.values.map(&:to_i)
     end
@@ -22,6 +29,12 @@ class EDTF
 
     members.each do |member|
       alias_method [member,'s'].join, member
+    end
+    
+    protected
+    
+    def parse(string)
+      string.match(ISO8601).captures.map(&:to_i)
     end
     
   end
