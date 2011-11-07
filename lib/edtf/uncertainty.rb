19ebebd2856c1625633e7e090f23dc8036616165
Sylvester Keil <sylvester@keil.or.at>
2011-11-07 15:31:08 +0100

    use splats instead of flatten

--- lib/edtf/uncertainty.rb
+++ lib/edtf/uncertainty.rb
@@ -1,66 +1,66 @@
 module EDTF
- 
+
   class Uncertainty < Struct.new(:year, :month, :day, :hour, :minute, :second)
-        
+
     def uncertain?(parts = members)
-      [parts].flatten.any? { |p| !!send(p) }
+      [*parts].any? { |p| !!send(p) }
     end
 
     def uncertain!(parts = members)
-      [parts].flatten.each { |p| send("#{p}=", true) }
+      [*parts].each { |p| send("#{p}=", true) }
       self
     end
 
     def certain?(parts = members); !uncertain?(parts); end
-    
+
     def certain!(parts = members)
-      [parts].flatten.each { |p| send("#{p}=", false) }
+      [*parts].each { |p| send("#{p}=", false) }
       self
     end
   end
 
   # year = []
   class Unspecified < Struct.new(:year, :month, :day)
-    
-		U = 'u'.freeze
-		
+
+    U = 'u'.freeze
+
     def initialize
-      super year = Array.new(4), month = Array.new(2), day = Array.new(2)
+      super Array.new(4),Array.new(2), Array.new(2)
     end
-    
+
     def unspecified?(parts = members)
-      [parts].flatten.any? { |p| send(p).any? { |u| !!u } }
+      [*parts].any? { |p| send(p).any? { |u| !!u } }
     end
-    
+
     def unspecified!(parts = members)
-      [parts].flatten.each { |p| send(p).map! { true } }
+      [*parts].each { |p| send(p).map! { true } }
       self
     end
 
     def specified?(parts = members); !unspecified?(parts); end
 
     def specified!(parts = members)
-      [parts].flatten.each { |p| send(p).map! { false } }
+      [*parts].each { |p| send(p).map! { false } }
       self
     end
-    
+
     alias specific? specified?
     alias unspecific? unspecified?
 
     alias specific! specified!
     alias unspecific! unspecified!
-    
+
     private :year=, :month=, :day=
-    
+
     def to_s
-			mask(%w{ ssss ss ss }).join('-')
+      mask(%w{ ssss ss ss }).join('-')
     end
-    
-		def mask(values)
-			values.zip(members.take(values.length)).map do |value, mask|
-				value.split(//).zip(send(mask)).map { |v,m| m ? U : v }.join
-			end
-		end
+
+    def mask(values)
+      values.zip(members.take(values.length)).map do |value, mask|
+        value.split(//).zip(send(mask)).map { |v,m| m ? U : v }.join
+      end
+    end
   end
-  
+
 end
