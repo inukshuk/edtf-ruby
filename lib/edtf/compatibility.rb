460313ebe3a5aca54f0e8a17010b3d5292be3414
Sylvester Keil <sylvester@keil.or.at>
2013-03-17 13:34:53 +0100

    ruby 1.8 compatibility

--- lib/edtf/compatibility.rb
+++ lib/edtf/compatibility.rb
@@ -1,8 +1,12 @@
 
 class DateTime
-
   def iso8601
     to_time.iso8601
   end unless method_defined?(:iso8601)
+end
 
+class Object
+  def <=>(other)
+    self === other ? 0 : nil
+  end unless method_deined?(:'<=>')
 end
